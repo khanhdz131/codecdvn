ce25d0b199d34ec78221a650244ac44027cf5f5f
khanhdz131 <101230523+khanhdz131@users.noreply.github.com>
2025-07-08 23:07:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 10000;
 
 // Setup static folder and views
 app.use(express.static(path.join(__dirname, "public")));
