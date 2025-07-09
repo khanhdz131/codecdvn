fb664ac41c9cf796546dc082d62dbb3d80825f61
khanhdz131 <101230523+khanhdz131@users.noreply.github.com>
2025-07-09 18:13:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -493,7 +493,7 @@ app.get('/', (req, res) => {
 
 
 // -------------------- CHẠY SERVER --------------------
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("✅ Server đang chạy tại http://localhost:" + PORT);
 });
